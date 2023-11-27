d7fb8f4231fb7e62a39011cf3462a85bf012d1e4
Matthew Kriel <matthew.k@skrmiish.com>
2023-11-27 16:23:03 +0200

    final push

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn habitat_project.wsgi --log-file -
--- habitat_project/settings.py
+++ habitat_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-47*&+h@+@y9c@eccj=2mret7(h4ts88)sr#ff)4_oq(%@rc7qn'
-
+# SECRET_KEY = 'django-insecure-47*&+h@+@y9c@eccj=2mret7(h4ts88)sr#ff)4_oq(%@rc7qn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.43', 'localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['https://habitat-6f899607bd9b.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -114,7 +114,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
